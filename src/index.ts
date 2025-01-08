5cd84aa97d9e8b35d8e93b3ea555f107317358ee
angemoudzouma <138227469+ange-tec@users.noreply.github.com>
2025-01-08 23:59:49 +0100

    project

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { CLI } from "./CLI";
+import inquirer from "inquirer"
+
 
 const startupParts = [
   "   __________  ____  ___       ____  ___    _   ____ __",
@@ -10,19 +12,40 @@ const startupParts = [
   "La banque de demain, aujourd'hui.",
   "",
 ];
-
 console.log(startupParts.join("\n"));
 
 // TODO
+const login = await inquirer.prompt([
+  {
+    type: 'number',
+    name: 'string',
+    message: 'entrez votre code pin'
+  }
+]);
+console.log("Votre code confidentiel est : " + login.type + login.name + login.message);
+
+const account = await  inquirer.prompt([
+  {
+    choices:[
+      "Déposer de l'argent",
+      "Retirer de l'argent",
+      "Consulter de l'historique"
+    ]
+  }
+])
 
 const cli = new CLI([
   {
+    answer: () => {
+
+    },
     title: "Créer un compte",
     value: "create",
     action: () => {
       console.log("Création d'un compte");
-    },
-  },
+      account.choices;
+    }
+  }
 ]);
 
 cli.menu();
